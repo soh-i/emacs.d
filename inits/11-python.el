63e1c5bc9440ded83be2793034e79ea218b7e5c1
soh-i <soh.ishiguro3914@gmail.com>
2013-08-05 01:08:28 +0900

    Add emacs-jedi

--- inits/11-python.el
+++ inits/11-python.el
@@ -1,3 +1,16 @@
+;; jedi
+(add-to-list 'load-path "~/.emacs.d/site-lisp/jedi/emacs-deferred")
+(add-to-list 'load-path "~/.emacs.d/site-lisp/jedi/emacs-epc")
+(add-to-list 'load-path "~/.emacs.d/site-lisp/jedi/emacs-ctable")
+(add-to-list 'load-path "~/.emacs.d/site-lisp/jedi/emacs-jedi")
+
+(require 'auto-complete-config)
+(require 'python)
+(require 'jedi)
+(add-hook 'python-mode-hook 'jedi:setup)
+(define-key python-mode-map (kbd "TAB") 'jedi:complete)
+;(setq jedi:complete-on-dot t)
+
 (add-hook 'python-mode-hook
           (lambda ()
             (define-key python-mode-map "\"" 'electric-pair)
@@ -14,3 +27,4 @@
      (define-key python-mode-map "\C-m" 'newline-and-indent)))
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 4)
+
