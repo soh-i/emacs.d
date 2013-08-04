a64512db0d85c91dc4a6f80a14e207ef6143db7f
soh-i <soh.ishiguro3914@gmail.com>
2013-08-04 18:10:43 +0900

    Add perl-completion

--- inits/10-perl.el
+++ inits/10-perl.el
@@ -1,3 +1,18 @@
+;; perl-completion
+(add-hook 'cperl-mode-hook
+          (lambda()
+            (require 'perl-completion)
+            (perl-completion-mode t)))
+
+(add-hook  'cperl-mode-hook
+           (lambda ()
+             (when (require 'auto-complete nil t) ; no error whatever auto-complete.el is not installed.
+               (auto-complete-mode t)
+               (make-variable-buffer-local 'ac-sources)
+               (setq ac-sources
+                     '(ac-source-perl-completion)))))
+
+
 ;; perl-tidy
 (defun perltidy-region ()
   "Run perltidy on the current region."
