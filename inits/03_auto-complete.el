330875c13c716e137a58528b98523e8b7cfa5ab6
soh-i <soh.ishiguro3914@gmail.com>
2013-08-07 11:09:27 +0900

    Fixed

--- inits/03_auto-complete.el
+++ inits/03_auto-complete.el
@@ -3,8 +3,8 @@
 (global-auto-complete-mode t)
 (setq ac-auto-start nil)
 (ac-set-trigger-key "TAB")
-(setq ac-use-fuzzy t)
 (add-to-list 'ac-dictionary-directories "/home/soh.i/.emacs.d/elpa/auto-complete-20130724.1750/dict")
 (ac-config-default)
-
+(setq ac-delay 0.0001)
+(setq ac-auto-show-menu 0.0001)
 
